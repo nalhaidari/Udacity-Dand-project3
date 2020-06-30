f7d5b3b7ca5dbd6f64c330aea49afe4cb543edcf
Nayef <n.alhaidari@hotmail.com>
2020-06-30 03:58:45 +0300

    adding create_tables.py

--- etl.py
+++ etl.py
@@ -2,18 +2,6 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries,create_table_queries,drop_table_queries
 
-
-def drop_tables(cur,conn):
-    for i, query in enumerate(drop_table_queries):
-        print(i)
-        cur.execute(query)
-        conn.commit()
-
-def create_tables(cur,conn):
-    for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
-
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
@@ -37,13 +25,6 @@ def main():
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
-    
-    print("droping tables ...")
-    drop_tables(cur, conn)
-    print("Done!!!")
-    print("Creating tables ...")
-    create_tables(cur, conn)
-    print ("done!!!")
     print("Loading staging tables ...")
     load_staging_tables(cur, conn)
     conn.commit()
